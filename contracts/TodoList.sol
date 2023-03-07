4d78e508a60f361be400cbf50ee374fd708979d7
Layne <huntlayne@icloud.com>
2023-03-07 11:17:13 -0700

    second commit set up clientside console

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,21 @@ pragma solidity "0.8.19";
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    function createTasks(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+    constructor() public {
+        createTasks("Start using this todo list!");
+    }
 }
